62c4d248f3c2acf29021f4c52cf85b43fd4a0ff2
Aman Bhargava <amanb2000@gmail.com>
2022-10-03 12:33:37 -0700

    Add functional direct_downloader script for yt-dlp

--- datasets/direct_downloader.py
+++ datasets/direct_downloader.py
@@ -41,6 +41,9 @@ parser.add_argument('--num-videos', action='store', type=int,
 		help="Number of videos to download from the source CSV. Selected randomly.", 
 		default= "10")
 
+parser.add_argument('--max-filesize', action='store', type=str, help="Max filesize (e.g., 100M).", 
+		default="100M")
+
 args = parser.parse_args()
 print("ARGS: \n\t", args)
 print("\n")
@@ -50,11 +53,69 @@ sub_dir = os.path.join(out_dir, 'subs')
 
 langs = 'en,zh-Hans,hi,es,fr,jp'
 
+## Name templates 
+template = "%(id)s.%(ext)s"
+vid_template = os.path.join(out_dir, template) 
+sub_template = os.path.join('subtitle:'+out_dir, 'subs', template)
+
+## Command manufacturing
+base_command = ["./yt-dlp", "--write-auto-subs", "--format-sort", "vext", "--format", "best[height<=360]"]
+base_command += ["--sub-langs", langs] 
+base_command += ["-o", vid_template]
+base_command += ["-o", sub_template] 
+base_command += ["--max-filesize", args.max_filesize]
+
+print("\nBASE COMMAND: \t", base_command)
+print("")
+
+
+## Getting dataframe 
+df = pd.read_csv(args.csv_path)
+df = df.sample(n=args.num_videos) #sampling
+
+URLS = df['video_id'].tolist()
+
+print("DATAFRAME: \n", df)
+print("URLS: ", URLS)
 
 
 
 ## Dispatch command 
-def download_url(url, opts):
+def download_url(url, base_command, base='downloads'):
 	# url is a string of the video id; opts is a dictionary with all the 
 	# commandline args for `yt-dlp` call. 
+	full_command = base_command.append(url)
+	full_command = base_command
+	# print(full_command)
 
+	print(f"Downloading {url}...")
+
+	if os.path.exists(os.path.join(base, url+".mp4")):
+		print(f"{url}.mp4 already exists in {base}! Skipping.")
+
+	x = subprocess.run(full_command, capture_output=True, text=True)
+	# print(x)
+	if x.returncode == 0: 
+		print(f"Done downloading {url}!\n")
+	else: 
+		print(f"ERROR WHEN DOWNLOADING {url}!!!")
+
+	return x
+
+
+num_naps = 0
+cnt = 0
+print("\n\n\nSTARTING DOWNLOADS!")
+for u in URLS: 
+	cnt+=1
+	retval = download_url(u, base_command, base=out_dir)
+
+	if retval != None and retval.returncode != 0: 
+		num_naps+=1
+		print(retval)
+		print(f"\n\nNap #{num_naps} of {cnt} tries...")
+		time.sleep(30)
+		print("Wakeup!\n")
+		# exit(0)
+
+	time.sleep(5)
